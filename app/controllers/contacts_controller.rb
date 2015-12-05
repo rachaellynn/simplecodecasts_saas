dbc080e581efabc48b27fee2ef9c110859414afa
rachaellynn <rachael@wordaroundtown.com>
2015-12-05 22:57:21 +0000

    updates to form alerts

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,5 +4,22 @@ class ContactsController < ApplicationController
     end
    
     def create
+        @contact = Contact.new(contact_params)
+        
+        if @contact.save
+            flash[:success] = "message sent"
+            redirect_to new_contact_path#, notice: "contact saved"
+        else
+            flash[:danger] = "error -- message has not been sent" #"error" seems to be deprecated
+            redirect_to new_contact_path#, notice: "error occurred"
+        end
     end
+    
+    
+    private
+    
+    def contact_params
+        params.require(:contact).permit(:name, :email, :comments)
+    end
+
 end
