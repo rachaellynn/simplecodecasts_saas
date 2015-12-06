fd431e6e449509d2d6e21cae712f384865a9a918
rachaellynn <rachael@wordaroundtown.com>
2015-12-06 15:03:11 +0000

    updates to html mailer in hopes of a fix

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
 class ContactMailer < ActionMailer::Base
-    default to: 'rachaellburger@gmail.com'
+    default to: 'rachael@wordaroundtown.com'
     
     def contact_email(name, email, body)
         @name = name
