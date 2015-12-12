91069f65abb0a6eea0f193c32f840c65b2ca27ae
rachaellynn <rachael@wordaroundtown.com>
2015-12-12 16:35:32 +0000

    added 'contact us' email functionality

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
 class ContactMailer < ActionMailer::Base
-    default to: 'rachael@wordaroundtown.com'
+    default to: 'rachaellburger@gmail.com'
     
     def contact_email(name, email, body)
         @name = name
