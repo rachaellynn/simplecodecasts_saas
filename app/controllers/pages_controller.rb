3fa0af33abe3fc62fce03e70e103a2ec90e88dbd
rachaellynn <rachael@wordaroundtown.com>
2015-12-24 04:37:53 +0000

    corrected syntax on pagescontroller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
     def home
-        @basic_plan = Find.plan(1) #allows us to access data about plans
-        @pro_plan = Find.plan(2) #allows us to access data about plan  
+        @basic_plan = Plan.find(1) #allows us to access data about plans
+        @pro_plan = Plan.find(2) #allows us to access data about plan  
     end
     
     def about
